1236ebe8d035d1d3368e3056d260923960bcf81f
Sander Mangel <sander@sandermangel.nl>
2017-10-03 12:03:45 +0200

    add unit test example

---
+++ 5.3.unit-testing/src/app/Sander/Module/Service/Fee.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sander\Module\Service;
+
+class Tax
+{
+    protected $taxRates = [
+        'nl' => 21,
+        'de' => 19,
+        'be' => 23
+    ];
+
+    /**
+     * @param string $country
+     * @return float
+     */
+    public function rateByCountry(string $country): float
+    {
+        return $taxRates[$country] ?? 0 ;
+    }
+
+    /**
+     * @param float $price
+     * @param string $country
+     * @return float
+     */
+    public function taxAmount(float $price, string $country): float
+    {
+        return $price * ($this->rateByCountry($country) / 100);
+    }
+}
---
+++ 5.3.unit-testing/src/tests/Unit/FeeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+
+use Sander\Module\Service\Tax;
+
+class TaxTest extends TestCase
+{
+    public function testTaxRatePerCountry()
+    {
+        $tax = new Tax();
+
+        $this->assertEquals(21, $tax->rateByCountry('nl'));
+        $this->assertEquals(19, $tax->rateByCountry('de'));
+        $this->assertEquals(23, $tax->rateByCountry('be'));
+    }
+
+    public function testZeroTaxOnUndefined()
+    {
+        $tax = new Tax();
+
+        $this->assertEquals(0, $tax->rateByCountry('unkown'));
+    }
+
+    public function testTaxAmountPerCountry()
+    {
+        $tax = new Tax();
+
+        $this->assertEquals(2.1, $tax->taxAmount(10,'nl'));
+        $this->assertEquals(1.9, $tax->taxAmount(10,'de'));
+        $this->assertEquals(2.3, $tax->taxAmount(10,'be'));
+    }
+}
